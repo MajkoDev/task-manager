24f783904838a4fcc8b573b07e555b5ae22a2a0b
Marian <hellomariandev@gmail.com>
2022-05-22 23:31:08 +0200

    set up app with header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  const headerStyle = {
+    color: "#ccc",
+    fontWeight: "400",
+    textAlign: "center",
+    margin: "50px 20px 60px 20px",
+  };
+
+  return <h1 style={headerStyle}>Task Manager</h1>;
+}
+
+export default Header;
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskInput() {
+  return (
+    <div>TaskInput</div>
+  )
+}
+
+export default TaskInput
